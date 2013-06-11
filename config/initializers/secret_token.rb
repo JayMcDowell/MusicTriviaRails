2e1366bf8a8019b01e7932dc30d0f4f29efb06fc
JayMcDowell <jay@musicianshalloffame.com>
2013-06-11 12:02:19 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MQR::Application.config.secret_token = 'ca41cd45bdd148efe5765f322b4ab8d7661875fe6c1106d70a45a9cfafd55547b415e3336221244025d4cd3548b5b0a35192d32a8107c7dd2b6a495bb6bb4596'
