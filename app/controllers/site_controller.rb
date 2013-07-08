34f62ed419d0457e0994a22d039fcf751d223d8c
JayMcDowell <jay@musicianshalloffame.com>
2013-07-08 12:55:29 -0500

    enabled quiz level choice

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -65,7 +65,7 @@ class SiteController < ApplicationController
     if session[:level_preference] == "medium"
       @question = quiz.questions.sample
     else
-      @question = quiz.questions.where(:quiz_level => session[:level_preference]).random
+      @question = quiz.questions.where(:quiz_level => session[:level_preference]).sample
     end
   end
 end
